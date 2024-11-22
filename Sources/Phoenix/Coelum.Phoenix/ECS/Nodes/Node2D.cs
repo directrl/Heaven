fc528819402d8bddd72821415ae5f70625f2064d
directrl <director@black-matter.org>
2024-11-22 17:31:34 +0100

    node2d/3d: helper properties

--- Sources/Phoenix/Coelum.Phoenix/ECS/Nodes/Node2D.cs
+++ Sources/Phoenix/Coelum.Phoenix/ECS/Nodes/Node2D.cs
@@ -1,12 +1,31 @@
+using System.Numerics;
 using Coelum.ECS;
 using Coelum.Phoenix.ECS.Components;
 
 namespace Coelum.Phoenix.ECS.Nodes {
 	
 	public class Node2D : Node {
+		
+		public Transform2D Transform { get; }
+
+		public Vector2 Position {
+			get => Transform.Position;
+			set => Transform.Position = value;
+		}
+		
+		public float Rotation {
+			get => Transform.Rotation;
+			set => Transform.Rotation = value;
+		}
+		
+		public Vector2 Scale {
+			get => Transform.Scale;
+			set => Transform.Scale = value;
+		}
 
 		public Node2D() {
-			AddComponent<Transform>(new Transform2D());
+			Transform = new();
+			AddComponent<Transform>(Transform);
 		}
 	}
 }
--- Sources/Phoenix/Coelum.Phoenix/ECS/Nodes/Node3D.cs
+++ Sources/Phoenix/Coelum.Phoenix/ECS/Nodes/Node3D.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Coelum.ECS;
 using Coelum.Phoenix.ECS.Components;
 
@@ -5,8 +6,26 @@ namespace Coelum.Phoenix.ECS.Nodes {
 	
 	public class Node3D : Node {
 
+		public Transform3D Transform { get; }
+
+		public Vector3 Position {
+			get => Transform.Position;
+			set => Transform.Position = value;
+		}
+		
+		public Vector3 Rotation {
+			get => Transform.Rotation;
+			set => Transform.Rotation = value;
+		}
+		
+		public Vector3 Scale {
+			get => Transform.Scale;
+			set => Transform.Scale = value;
+		}
+		
 		public Node3D() {
-			AddComponent<Transform>(new Transform3D());
+			Transform = new();
+			AddComponent<Transform>(Transform);
 		}
 	}
 }
